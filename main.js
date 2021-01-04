254fbdd71e9bd67181e09c6004a46dd638c213f6
Tobias <twadseth@icloud.com>
2021-01-04 22:37:20 +0100

    Added opening and saving files and a menu

--- main.js
+++ main.js
@@ -1,14 +1,199 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, MenuItem, ipcMain, dialog } = require('electron')
+const fs = require('fs')
+
+let confirmed = false
+
+let mainWindow
+
+let unsavedChanges
 
 app.whenReady().then(() => {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true,
-            devTools: false
+            nodeIntegration: true
         }
     })
     mainWindow.setTitle('Markdown Editor')
     mainWindow.loadFile('./public/index.html')
+
+    const createNewFile = content => {
+        dialog.showSaveDialog(mainWindow, {
+            title: 'Create New File',
+            properties: ['showOverwriteConfirmation'],
+            filters: [
+                {
+                    name: 'Markdown Files',
+                    extensions: ['md']
+                }
+            ]
+        }).then(({ canceled, filePath }) => {
+            if (canceled) return
+
+            fs.writeFile(filePath, content, err => {
+                if (err) return
+
+                mainWindow.webContents.send('fileopened', {
+                    path: filePath,
+                    content
+                })
+            })
+        })
+    }
+
+    const openFile = () => {
+        const file = dialog.showOpenDialogSync(mainWindow, {
+            properties: ['openFile'],
+            filters: [{ name: 'Markdown Files', extensions: ['md'] }]
+        })
+
+        if (file) {
+            fs.readFile(file[0], 'utf-8', (err, data) => {
+                if (err) return
+
+                mainWindow.webContents.send('fileopened', {
+                    path: file[0],
+                    content: data
+                })
+            })
+        }
+    }
+
+    ipcMain.on('savenewfile', (e, content) => {
+        createNewFile(content)
+        e.reply('update', content)
+    })
+
+	// TODO: Fix the unsaved changes for saveexistingfile
+    ipcMain.on('saveexistingfile', (e, { path, content }) => {
+        fs.writeFile(path, content, err => {
+            if (err) return
+        })
+        e.reply('update', content)
+    })
+
+    ipcMain.on('unsavedchanges-reply', (e, msg) => {
+        unsavedChanges = msg.state
+    })
+
+    const menu = new Menu()
+
+    menu.append(
+        new MenuItem({
+            role: 'appMenu'
+        })
+    )
+
+    menu.append(
+        new MenuItem({
+            role: 'fileMenu',
+            submenu: [
+                {
+                    label: 'New File',
+                    accelerator: 'CmdOrCtrl+N',
+                    click: () => createNewFile()
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Open...',
+                    accelerator: 'CmdOrCtrl+O',
+                    click: openFile
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Save',
+                    accelerator: 'CmdOrCtrl+S',
+                    click: () => mainWindow.webContents.send('savefile')
+                },
+                {
+                    type: 'separator'
+                }
+            ]
+        })
+    )
+
+    menu.append(
+        new MenuItem({
+            role: 'editMenu'
+        })
+    )
+
+    menu.append(
+        new MenuItem({
+            role: 'viewMenu'
+        })
+    )
+
+    menu.append(
+        new MenuItem({
+            role: 'windowMenu'
+        })
+    )
+
+    menu.append(
+        new MenuItem({
+            role: 'helpMenu'
+        })
+    )
+
+    Menu.setApplicationMenu(menu)
+
+    mainWindow.on('close', e => {
+        if (!confirmed) {
+            if (unsavedChanges) {
+                dialog.showMessageBox(null, {
+                    type: 'warning',
+                    buttons: ['Cancel', 'Save', 'Don\'t Save'],
+                    defaultId: 1,
+                    message: 'Do you want to save the changes you made?',
+                    detail: 'Your changes will be lost if you don\'t save them.'
+                }).then(data => {
+                    let res = data.response
+                    if (res === 0) {
+                        return
+                    } else if (res === 1) {
+                        confirmed = true
+                        mainWindow.webContents.send('savefile')
+                        mainWindow.close()
+                    } else {
+                        confirmed = true
+                        mainWindow.close()
+                    }
+                })
+                e.preventDefault()
+            }
+        }
+    })
+})
+
+app.on('before-quit', e => {
+    if (!confirmed) {
+        if (unsavedChanges) {
+            dialog.showMessageBox(null, {
+                type: 'warning',
+                buttons: ['Cancel', 'Save', 'Don\'t Save'],
+                defaultId: 1,
+                message: 'Do you want to save the changes you made?',
+                detail: 'Your changes will be lost if you don\'t save them.'
+            }).then(data => {
+                let res = data.response
+                if (res === 0) {
+                    return
+                } else if (res === 1) {
+                    confirmed = true
+                    mainWindow.webContents.send('savefile')
+                    mainWindow.close()
+                } else {
+                    confirmed = true
+                    mainWindow.close()
+                }
+            })
+            e.preventDefault()
+        }
+    }
 })
 
 app.on('window-all-closed', () => {
--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,42 @@
 	import Preview from './Preview.svelte'
 
 	let markdown = ''
+	let fileContent
 
 	let activeFilePath
 
-	const { ipcRenderer } = require('electron')
+	const { ipcRenderer, BrowserWindow } = require('electron')
 
-	ipcRenderer.on('savefile', () => {
-		// Save file
+	ipcRenderer.on('savefile', (e) => {
+		if (activeFilePath) {
+			ipcRenderer.send('saveexistingfile', {
+				path: activeFilePath,
+				content: markdown
+			})
+		} else {
+			ipcRenderer.send('savenewfile', markdown)
+		}
 	})
+
+	// TODO: Fix the unsaved changes for saveexistingfile
+	ipcRenderer.on('update', (e, { content }) => {
+		fileContent = content
+	})
+	
+	ipcRenderer.on('fileopened', (e, { path, content }) => {
+		activeFilePath = path
+		markdown = content
+		fileContent = content
+		unsavedChanges = false
+	})
+
+	$: unsavedChanges = !(fileContent === markdown)
+	let lastState = true
+	$: if (unsavedChanges === lastState) {
+		const { port1, port2 } = new MessageChannel()
+		ipcRenderer.postMessage('unsavedchanges-reply', { state: unsavedChanges }, [port1])
+		lastState = !lastState
+	}
 </script>
 
 <style>
@@ -31,7 +59,7 @@
 
 <main>
 	<p class="file-path">
-		{activeFilePath ? activeFilePath : "Press 'Save' or hit 'CTRL + S' to save"}
+		{activeFilePath ? activeFilePath.split('/')[activeFilePath.split('/').length - 1] : "Untitled File"}{unsavedChanges ? '*' : ''}
 	</p>
 	<p class="editor-and-preview">
 		<Editor bind:markdown />
